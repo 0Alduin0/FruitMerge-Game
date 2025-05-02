8a6f4ca8cf37199bed33cd2b25a7344e589cd096
Enes Yürekli <muhammedenesyurekli@gmail.com>
2025-05-02 22:35:48 +0300

    ana ses slider eklendi

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public GameObject settingsScreen;
+
+    public void SettingsScreen()
+    {
+        settingsScreen.SetActive(true);
+    }
+}
